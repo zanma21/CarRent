c2739c49aef22cadd01dfcfd492e37ae5deab0a1
zanma21 <mario.zanni21@icloud.com>
2022-09-12 16:47:59 +0200

    alles aktualisiert

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CarRent
-Testat: Mini-Projekt Aufgabe
+# carrent
+
+[![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=zbw-menzi_carrent&metric=code_smells)](https://sonarcloud.io/summary/new_code?id=zbw-menzi_carrent)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=zbw-menzi_carrent&metric=coverage)](https://sonarcloud.io/summary/new_code?id=zbw-menzi_carrent)
--- source/src/Program.cs
+++ source/src/Program.cs
@@ -1,13 +1,28 @@
+using System.Configuration;
+
+using CarRent.Car.Domain;
+using CarRent.Car.Infrastructure.Persistence;
+using CarRent.Car.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = "Server=(localdb)\\mssqllocaldb;Database=CarRent;Trusted_Connection=True;MultipleActiveResultSets=true";
+builder.Services.AddDbContext<CarContext>(x => x.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseDeveloperExceptionPage();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
